3d92ae89613463bc33cdf49950586e1235c5418e
git504 <git504@mailo.com>
2021-10-02 15:47:44 +0200

    fetch data

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ fetch("http://localhost:3000/photographers")
   .then((res) => {
     if (res.ok) return res.json();
   })
-  .catch((err) => console.err("Une erreur est survenue:", err))
+  .catch((err) => console.error("Une erreur est survenue:", err))
   .then((res) => console.log("Success:", JSON.stringify(res)));
 
 /*
