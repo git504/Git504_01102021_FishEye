b46ad1463b848287f6a8131cb2d7318aba15165e
git504 <git504@mailo.com>
2021-10-02 15:45:49 +0200

    prevent default

--- app.js
+++ app.js
@@ -1,24 +1,11 @@
 "use strict";
-// import fetch from "node-fetch";
-// const fetch = require("node-fetch");
-
-// fetch("http://localhost:3000/api")
-//   .then(function (res) {
-//     if (res.ok) {
-//       return res.json();
-//     }
-//   })
-//   .then(function (value) {
-//     console.log(value);
-//   })
-//   .catch(function (err) {
-//     // Une erreur est survenue
-//   });
 
 fetch("http://localhost:3000/photographers")
-  .then((response) => response.json())
-  .catch((error) => console.error("Error:", error))
-  .then((response) => console.log("Success:", JSON.stringify(response)));
+  .then((res) => {
+    if (res.ok) return res.json();
+  })
+  .catch((err) => console.err("Une erreur est survenue:", err))
+  .then((res) => console.log("Success:", JSON.stringify(res)));
 
 /*
 \   ^__^
