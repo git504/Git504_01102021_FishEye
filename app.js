8c49087646f056e3edef03620f4945dc1e60141c
git504 <git504@mailo.com>
2021-10-04 13:58:35 +0200

    webpack + loader babel

--- app.js
+++ app.js
@@ -1,12 +1,3 @@
-"use strict";
-
-fetch("http://localhost:3000/photographers")
-  .then((res) => {
-    if (res.ok) return res.json();
-  })
-  .catch((err) => console.error("Une erreur est survenue :", err))
-  .then((res) => console.log("Success:", JSON.stringify(res)));
-
 /*
 \   ^__^
 \  (oo)\_______
@@ -14,3 +5,41 @@ fetch("http://localhost:3000/photographers")
        ||----w |
        ||     ||
 */
+"use strict";
+const URL_PHOTOGRAPHERS = "http://localhost:3000/photographers";
+const URL_MEDIA = "http://localhost:3000/media";
+
+// fetch()retourne une Promise. then() et catch()sont appelées plus tard.
+async function fetchPhotographersJSON() {
+  const responsePhotographers = await fetch("URL_PHOTOGRAPHERS");
+  if (responsePhotographers.ok) {
+    const photographers = await res.json();
+    return photographers;
+  }
+}
+fetchPhotographersJSON()
+  .then((photographers) =>
+    console.log("Success:", JSON.stringify(photographers))
+  )
+  .then((photographers) => {
+    photographers; // fetched photographers
+  })
+  .catch((err) => console.error("Une erreur est survenue :", err));
+
+// const responseMedia = await fetch("/media");
+// const media = await res.json();
+// return media;
+
+async function fonctionAsynchrone1() {
+  /* code asynchrone */
+}
+async function fonctionAsynchrone2() {
+  /* code asynchrone */
+}
+
+async function fonctionAsynchrone3() {
+  const value1 = await fonctionAsynchrone1();
+  const value2 = await fonctionAsynchrone2();
+  return value1 + value2;
+}
+// https://openclassrooms.com/fr/courses/5543061-ecrivez-du-javascript-pour-le-web/5866911-parallelisez-plusieurs-requetes-http
