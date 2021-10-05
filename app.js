c51f3b4a76cfba65d0692e221c795aa202b0484d
git504 <git504@mailo.com>
2021-10-05 14:23:43 +0200

    promiseall

--- app.js
+++ app.js
@@ -2,17 +2,18 @@
 const URL_PHOTOGRAPHERS = "http://localhost:3000/photographers";
 const URL_MEDIA = "http://localhost:3000/media";
 
-// La fonction   Promise.all, exécute nos requêtes en parallèle et en séquence.
-
-Promise.all([fetch(URL_PHOTOGRAPHERS), fetch(URL_MEDIA)])
-  .then(async ([photographers, media]) => {
-    const A_FETCH = await photographers.json();
-    const B_FETCH = await media.json();
-    return [A_FETCH, B_FETCH];
-  })
-  .then((res) => {
-    console.log(res);
-  })
-  .catch((err) => {
-    console.log(err, "Une erreur est survenue");
-  });
+// Promise.all, exécute nos requêtes en parallèle et en séquence.
+var requests = async () => {
+  await Promise.all([fetch(URL_PHOTOGRAPHERS), fetch(URL_MEDIA)])
+    .then(([photographers, media]) => {
+      const A_FETCH = photographers.json();
+      const B_FETCH = media.json();
+      return [A_FETCH, B_FETCH];
+    })
+    .then((res) => {
+      console.log(res);
+    })
+    .catch((err) => {
+      console.log(err, "Une erreur est survenue");
+    });
+};
