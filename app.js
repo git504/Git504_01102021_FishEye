ea31e669c272c54f9c967a0018937da7bdc9de55
git504 <git504@mailo.com>
2021-11-04 09:22:46 +0100

    USER show

--- app.js
+++ app.js
@@ -2,12 +2,7 @@ console.log("%c APP status ok", "color: green; font-weight:bold;");
 ("use strict");
 
 import * as myFilterModule from "./filtertags.js";
-// import * as myModalModule from "./modal.js";
-// import * as mySliderModule from "./slider.js";
-import * as myLikesModule from "./likes.js";
 import * as myUsersModule from "./displayusers.js";
-import * as myUserModule from "./displayuser.js";
-import * as myDropdownModule from "./dropdown.js";
 import * as myAnchorModule from "./onscroll.js";
 
 const photographers = myUsersModule.getPhotographers();
--- displayuser.js
+++ displayuser.js
@@ -1,63 +1,74 @@
 import * as myFetchModule from "./fetch.js";
-import { getPhotographers } from "./displayusers.js";
+import * as myUsersModule from "./displayusers.js";
+import * as myModalModule from "./modal.js";
+import * as mySliderModule from "./slider.js";
+import * as myDropdownModule from "./dropdown.js";
+import * as myLikesModule from "./likes.js";
+
 ("use strict");
 
-let photographer = {};
+let photographersOnUserPage = {};
+let mediaItemOnUserPage = {};
+
+const dom = {
+  inputSelect: document.querySelector(".filter__select"),
+  photographerHeader: document.querySelector(".user--page"),
+  nameInForm: document.querySelector(".modal__head"),
+  media: document.querySelector(".media"),
+  form: document.querySelector(".form"),
+  modalForm: document.querySelector(".modal"),
+  selectMenu: document.querySelector(".filter__custom-menu"),
+  selectOption: document.querySelectorAll(".filter__custom-option"),
+};
+// console.log(dom);
 
 // To get Id in Url's params
 const params = new URLSearchParams(window.location.search);
 let getPhotographerById = params.get("id");
-
-console.log(myFetchModule.getUrl());
+// console.log(getPhotographerById);
 
 // To display photographer header
-console.log(getPhotographerById);
+// SI API => console.log(`${myFetchModule.getUrl()}/${getPhotographerById}`);
 const showUserHeader = async () => {
-  try {
-    // + "/photographers/id/"}/${getPhotographerById}
-    let reponse = await fetch(`${myFetchModule.getUrl()}`);
-    if (reponse.ok) {
-      photographer = await reponse.json();
-      console.log(
-        "%c DB_pageProfil_statusOK",
-        "color: green; font-weight:bold;",
-        photographer
-      );
-    }
-  } catch (error) {
-    console.log(
-      "ERREUR LORS DE LA RECUPERATION DE LA DB PAGE user.html",
-      error
-    );
-  }
+  const db = await myFetchModule.getDatas();
+  const photographers = db.photographers;
+  const media = db.media;
+  // console.log(photographers, media);
+
+  photographers.map((photographer) => {
+    photographersOnUserPage = {
+      city: photographer.city,
+      country: photographer.country,
+      id: photographer.id,
+      name: photographer.name,
+      portrait: photographer.portrait,
+      price: photographer.price,
+      tagline: photographer.tagline,
+      tags: photographer.tags,
+    };
+    console.log(photographersOnUserPage);
+  });
+
+  media.map((item) => {
+    mediaItemOnUserPage = {
+      altTxt: item.altTxt,
+      date: item.date,
+      id: item.id,
+      image: item.image,
+      likes: item.likes,
+      photographerId: item.photographerId,
+      price: item.price,
+      tags: item.tags,
+      title: item.title,
+    };
+    console.log(mediaItemOnUserPage);
+  });
+
+  //Présentation des photopgraphes
+
+  userHeader = `
+
+
+  `;
 };
-
 showUserHeader();
-
-// getPhotographers().then((photographers) => {
-//   const idOfPhotographers = photographers.filter((photographer) => {
-//     console.log(photographer.id);
-//     return photographer.id;
-//   });
-// });
-
-// const dom = {
-//   inputSelect: document.querySelector(".filter__select"),
-//   photographerHeader: document.querySelector(".user--page"),
-//   nameInForm: document.querySelector(".modal__head"),
-//   media: document.querySelector(".media"),
-//   form: document.querySelector(".form"),
-//   modalForm: document.querySelector(".modal"),
-//   selectMenu: document.querySelector(".filter__custom-menu"),
-//   selectOption: document.querySelectorAll(".filter__custom-option"),
-// };
-
-// const getMedias = async () => {
-//   const db = await myFetchModule.getDatas();
-//   const medias = db.media;
-// console.log(medias);
-// };
-
-// export { getMedias };
-
-// let photographerPage = "";
