e9d34bceb1af827c35db97bd03a2b1a9c12b9f90
git504 <git504@mailo.com>
2021-11-12 16:01:58 +0100

    bug comit

--- dropdown.js
+++ dropdown.js
@@ -1,3 +1,28 @@
 // console.log("DROPDOWN.JS FONCTIONNE");
 
 ("use strict");
+
+let sortMediaByFilter = (media, filter) => {
+  switch (filter) {
+    case "likes":
+      return media.sort((a, b) => {
+        return b[filter] - a[filter];
+      });
+    case "title":
+      return media.sort((a, b) => {
+        if (a[filter] < b[filter]) {
+          return -1;
+        }
+        if (a[filter] > b[filter]) {
+          return 1;
+        }
+        return 0;
+      });
+    case "date":
+      return media.sort((a, b) => {
+        return new Date(b[filter]) - new Date(a[filter]);
+      });
+    default:
+      break;
+  }
+};
--- likes.js
+++ likes.js
@@ -1,2 +1,39 @@
 // console.log("LIKE.JS FONCTIONNE");
 ("use strict");
+
+// Compteur de likes
+export const increaseOrDecreaseLikesAndTotalLikes = () => {
+  const likesElements = document.querySelectorAll(".media__likes");
+  likesElements.forEach((like) => {
+    ["click", "keyup"].forEach((event) =>
+      like.addEventListener(event, (e) => {
+        let totalLikes = document.querySelector(".infos__totalLikes");
+        if (e.key == "Enter") {
+          if (!like.classList.contains("media__liked")) {
+            increaseLikesAndTotalLikes(totalLikes, like);
+          } else {
+            decreaseLikesAndTotalLikes(totalLikes, like);
+          }
+        } else if (e.type == "click") {
+          if (!like.classList.contains("media__liked")) {
+            increaseLikesAndTotalLikes(totalLikes, like);
+          } else {
+            decreaseLikesAndTotalLikes(totalLikes, like);
+          }
+        }
+      })
+    );
+  });
+};
+
+export const increaseLikesAndTotalLikes = (totalLikes, like) => {
+  ++totalLikes.innerText;
+  ++like.children[0].innerText;
+  like.classList.add("media__liked");
+};
+
+export const decreaseLikesAndTotalLikes = (totalLikes, like) => {
+  --totalLikes.innerText;
+  --like.children[0].innerText;
+  like.classList.remove("media__liked");
+};
