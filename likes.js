da6ecf8b9b25f8b143a6607874753856c3adfc69
git504 <git504@mailo.com>
2021-11-23 11:17:07 +0100

    changemenufilter

--- likes.js
+++ likes.js
@@ -26,16 +26,19 @@ export const increaseOrDecreaseLikesAndTotalLikes = () => {
     );
   });
 };
-
 export const increaseLikesAndTotalLikes = (totalLikes, like) => {
   // console.log(totalLikes);
   ++totalLikes.innerText;
   ++like.previousElementSibling.innerText;
   like.classList.add("media__liked");
+  // like.firstChild.nextElementSibling.setAttribute("fill", "#901c1c");
+  // console.log(like.firstChild.nextElementSibling);
 };
 
 export const decreaseLikesAndTotalLikes = (totalLikes, like) => {
   --totalLikes.innerText;
   --like.previousElementSibling.innerText;
   like.classList.remove("media__liked");
+  // console.log(like.firstChild.nextElementSibling);
+  // like.firstChild.nextElementSibling.setAttribute("fill", "grey");
 };
